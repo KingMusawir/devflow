8d7a590da675ec8449a314872942ec75340a5fef
Musawir <musawir.raji@gmail.com>
2024-12-06 01:00:03 -0800

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 const Home = () => {
-  return (
-    <h1 className="text-5xl text-violet-600">
-      Welcome to the world of Next js
-    </h1>
-  );
+    return (
+        <h1 className="text-5xl font-semibold text-violet-600">
+            Welcome to the world of Next js
+        </h1>
+    );
 };
 
 export default Home;
