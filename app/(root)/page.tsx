0bdd8b7a604677b64370e402baff6944341598d3
Musawir <musawir.raji@gmail.com>
2024-12-09 07:51:32 -0800

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and Github Provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-const Home = () => {
+import { auth } from '@/auth';
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <>
       <h1 className="text-5xl font-semibold text-violet-600">
---
+++ constants/routes.ts
@@ -0,0 +1,10 @@
+const ROUTES = {
+  HOME: '/',
+  SIGN_UP: '/sign-up',
+  SIGN_IN: '/sign-in',
+  // FORGOT_PASSWORD: '/forgot-password',
+  // RESET_PASSWORD: '/reset-password',
+  // VERIFY_EMAIL: '/verify-email',
+  // VERIFY_EMAIL_SUCCESS: '/verify-email/success',
+};
+export default ROUTES;
