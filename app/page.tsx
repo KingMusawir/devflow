705366303c51604915aef236cd9cc0789d20674a
Musawir <musawir.raji@gmail.com>
2024-12-09 03:54:53 -0800

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 const Home = () => {
-    return (
-        <>
-            <h1 className="text-5xl font-semibold text-violet-600">
-                Welcome to the world of Next js
-            </h1>
-            <h1 className="font-space-grotesk text-5xl text-yellow-400">
-                Welcome to the world of Next js
-            </h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className="text-5xl font-semibold text-violet-600">
+        Welcome to the world of Next js
+      </h1>
+    </>
+  );
 };
 
 export default Home;
