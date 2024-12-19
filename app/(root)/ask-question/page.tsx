4d27e5396890082b075fb73d6de7ead1bf274c74
Musawir <musawir.raji@gmail.com>
2024-12-18 20:10:17 -0800

    Implement the UI if QuestionForm

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import QuestonForm from '@/components/forms/QuestonForm';
 
 const AskQuestions = () => {
-  return <div>AskQuestions</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestonForm />
+      </div>
+    </div>
+  );
 };
 export default AskQuestions;
